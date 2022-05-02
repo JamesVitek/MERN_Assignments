d72fcfeddb206034c22767cbb2f5a007c133d1b5
James Vitek <vitek96@gmai.com>
2022-05-02 16:36:54 -0400

    Predict Destructing

---
+++ Predict_Destructuring.js
@@ -0,0 +1,93 @@
+// const cars = ['Tesla', 'Mercedes', 'Honda']
+// const [ randomCar ] = cars
+// const [ ,otherRandomCar ] = cars
+// //Predict the output
+// console.log(randomCar)
+// console.log(otherRandomCar)
+
+/*
+random car = Tesla
+other random car = Mercedes
+
+-Correct
+
+============================= 1
+*/
+
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// }
+// const { name: otherName } = employee;
+// //Predict the output
+// console.log(name);
+// console.log(otherName);
+
+/*
+printing name errors
+
+-correct - I have a JS plugin that crossed out name for me xD
+
+============================= 2
+*/
+
+// const person = {
+//     name: 'Phil Smith',
+//     age: 47,
+//     height: '6 feet'
+// }
+// const password = '12345';
+// const { password: hashedPassword } = person;  
+// //Predict the output
+// console.log(password);
+// console.log(hashedPassword);
+
+/*
+printing 12345
+then undefined because it doesnt go into person
+
+-correct
+
+============================= 3
+*/
+
+// const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+// const [,first] = numbers;
+// const [,,,second] = numbers;
+// const [,,,,,,,,third] = numbers;
+// //Predict the output
+// console.log(first == second);
+// console.log(first == third);
+
+/*
+Errors because numbers is const
+
+-wrong, prints false then true
+
+============================= 4
+*/
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+/*
+prints value
+prints [1,5,1,8,3,3]
+prints 1
+prints 5
+
+-wrong, prints false then true
+
+============================= 5
+*/
