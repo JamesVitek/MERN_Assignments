aa9ed36906d214bff47a83a914ead13f455c7a51
James Vitek <101815798+JamesVitek@users.noreply.github.com>
2022-05-20 13:27:22 -0400

    Delete Exam/authors directory
    Need to post new upload

--- Exam/authors/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://root:root@clustermay.uiass.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('Established a connection to the database'))
-    .catch(err => console.log('Something went wrong when connecting to the database ', err));
